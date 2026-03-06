0e24ef07caa63008cc1a355a8f2b1a7747d24ed2
Lyle Smal - Onsight <lyle@onsightapp.com>
2026-03-06 19:43:51 +0200

    UI changes, themes, fonts

---
+++ MyFirstApp.Services/Implementations/ExpenseService/ExpenseService.cs
@@ -0,0 +1,33 @@
+﻿using MyFirstApp.Domain.Models;
+using MyFirstApp.Services.Implementations.PublisherService;
+using MyFirstApp.Services.Interfaces;
+using MyFirstApp.Services.Repositories;
+
+namespace MyFirstApp.Services.Implementations.ExpenseService
+{
+    public class ExpenseService : PublisherService<ExpenseModel>, IExpenseService
+    {
+        private readonly ExpenseRepository _expenseRepository;
+        private readonly IMonthlyBudgetFixtureService _monthlyBudgetFixtureService;
+
+        public ExpenseService(ExpenseRepository expenseRepository, IMonthlyBudgetFixtureService monthlyBudgetFixtureService)
+        {
+            _expenseRepository = expenseRepository;
+            _monthlyBudgetFixtureService = monthlyBudgetFixtureService;
+        }
+
+        public async Task SubscribeAsync(Func<ExpenseModel, Task> handler)
+        {
+            Subscribe(handler);
+
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateExpenseAsync(ExpenseModel expenseModel)
+        {
+            await _expenseRepository.UpdateAsync(expenseModel);
+            await _monthlyBudgetFixtureService.RecalculateAndUpdateBudgetFixtureAsync();
+            await Publish(expenseModel);
+        }
+    }
+}
---
+++ MyFirstApp.Services/Interfaces/IExpenseService.cs
@@ -0,0 +1,11 @@
+﻿using MyFirstApp.Domain.Models;
+using MyFirstApp.Services.Interfaces.PublisherService;
+
+namespace MyFirstApp.Services.Interfaces
+{
+    public interface IExpenseService : IPublisher<ExpenseModel>
+    {
+        Task SubscribeAsync(Func<ExpenseModel, Task> handler);
+        Task UpdateExpenseAsync(ExpenseModel expenseModel);
+    }
+}
